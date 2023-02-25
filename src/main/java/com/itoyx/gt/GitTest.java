a318c081b782b033fabceb2c05656c27efbbe581
YONSEN <oyx20020328@gmail.com>
2023-02-25 11:52:26 +0800

    git-test hot-fix c2

--- src/main/java/com/itoyx/gt/GitTest.java
+++ src/main/java/com/itoyx/gt/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Github");
         System.out.println("Hello Gitee");
         System.out.println("Hello GitLab");
+        System.out.println("Hello Java");
     }
 }
